c8418adff0b1bc4129d4ee253f95a897010dc1ec
Alaka1996 <bgalakananda@gmail.com>
2024-12-23 12:38:59 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 #include "utils.h"
 
 int main() {
-    printf("Hello from main!\n");
+    printf("Hello from main\n");
     int result = add(5, 3);
     printf("The result of 5 + 3 is: %d\n", result);
     return 0;
